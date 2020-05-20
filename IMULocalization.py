e7c228333982f26292bb426f32bb68bfbf5ac030
phamr39 <pha.mr3998@gmail.com>
2020-05-20 15:36:53 +0700

    test_2

--- IMULocalization.py
+++ IMULocalization.py
@@ -187,18 +187,21 @@ class CommUART:
             stopbits=serial.STOPBITS_ONE,
         )
         while(1):
-            pre_traj = []
-            p_traj = []
-            p_traj = Tools.ReadFile('/home/swarm-robotics/path_planning/data/waypoint.txt')
-            # trajectory_changed = random.randint(0,1)
-            if (p_traj.difference(pre_traj) != ''):
-                n = 'S'
-                for kjh in range(0,10):
-                    serial_port.write(n.encode())
-                CommUART.UARTSend(p_traj)
-            else:
-                CommUART.UARTReceive()
-            pre_traj = p_traj
+            try:
+                pre_traj = []
+                p_traj = []
+                p_traj = Tools.ReadFile('/home/swarm-robotics/path_planning/data/waypoint.txt')
+                # trajectory_changed = random.randint(0,1)
+                if (p_traj.difference(pre_traj) != ''):
+                    n = 'S'
+                    for kjh in range(0,10):
+                        serial_port.write(n.encode())
+                    CommUART.UARTSend(p_traj)
+                else:
+                    CommUART.UARTReceive()
+                pre_traj = p_traj
+            except:
+                print("No new trajectory detected ")
     def engine_test():
         while(1):
             CommUART.UARTReceive()
